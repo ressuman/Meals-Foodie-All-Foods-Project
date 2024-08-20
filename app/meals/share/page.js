81e77c1faec9431762c1a850e1ca7ad9ebe68f7d
Richard Essuman <ressuman001@gmail.com>
2024-08-20 21:27:18 +0000

    "Added share meal functionality with image upload, validation, and database storage"

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,6 +1,15 @@
+//"use client";
+
+import ImagePicker from "@/components/meals/image-picker/image-picker";
 import classes from "./page.module.css";
+import { shareMeal } from "@/lib/actions/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit/meals-form-submit";
+//import { useFormState } from "react-dom";
 
 export default function MealsSharePage() {
+  //const status=useFormStatus()
+  //status.pending
+
   return (
     <>
       <header className={classes.header}>
@@ -10,7 +19,7 @@ export default function MealsSharePage() {
         <p>Or any other meal you feel needs sharing!</p>
       </header>
       <main className={classes.main}>
-        <form className={classes.form}>
+        <form className={classes.form} action={shareMeal}>
           <div className={classes.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -38,9 +47,9 @@ export default function MealsSharePage() {
               required
             ></textarea>
           </p>
-          IMAGE PICKER
+          <ImagePicker label="Your Image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
